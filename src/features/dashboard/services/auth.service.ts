5bd5516e7c2ef7e389654e6c47aa6267c6503a2d
Pablo Miranda <pablin330@gmail.com>
2026-07-02 20:08:40 -0500

    feat: add login form

---
+++ src/features/dashboard/services/auth.service.ts
@@ -0,0 +1,10 @@
+import { api } from '@/lib/https'
+import { redirect } from "next/navigation";
+import { LoginForm } from '../types/login-form.type'
+
+
+export async function login(formData: LoginForm) {
+  
+
+
+}
